cfa459baf784530aa59c07caa44019640d70bc9a
Flávio <flavioandrade0201@gmail.com>
2025-03-01 18:14:40 +0900

    updated to latest openai

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -1,19 +1,23 @@
 import openai
+from openai import OpenAI
 from decouple import config
-
 from functions.database import get_recent_messages
 
-openai.organization = config("OPEN_AI_ORG")
-openai.api_key = config("OPEN_AI_KEY")
+# TODO: The 'openai.organization' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(organization=config("OPEN_AI_ORG"))'
+# openai.organization = config("OPEN_AI_ORG")
+
+client = OpenAI(api_key=config("OPEN_AI_KEY"))
 
 
 # convert audio to text
 def convert_audio_to_text(audio_file):
     try:
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        print("Transcript returned:", transcript)  # Debug output
-        message_text = transcript.get("text")
-        return message_text
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+        print("Transcript returned:", transcript.text)  # Debug output
+        return transcript.text
     except Exception as e:
         print(f"Error during transcription: {e}")
         return None
@@ -29,11 +33,9 @@ def get_chat_response(message_input):
     print(messages)
 
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
-        message_text = response["choices"][0]["message"]["content"]
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+                                                  messages=messages)
+        message_text = response.choices[0].message.content
         return message_text
     except Exception as e:
         print(e)
